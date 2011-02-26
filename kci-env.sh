426f9b09e6db35df93234f1b5974fb13088b4878
Venkatraman S <svenkatr@gmail.com>
2011-02-26 12:16:11 +0530

    More variables to simulate KCI environment

--- kci-env.sh
+++ kci-env.sh
@@ -6,6 +6,8 @@ export ARCH=
 export CROSS_COMPILE=
 
 #Set variables as expected in the CI environment, for testing
+export RESOURCES_URL=http://omaplbp.india.ti.com:8000
 export WORKSPACE=`pwd`/..
 export JOB_NAME=$WORKSPACE/..
 export BUILD_ID=999
+export NODE_NAME=local
