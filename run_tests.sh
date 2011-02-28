070d37f911ae9ca245aa74429d6c68111bb059fc
Venkatraman S <svenkatr@gmail.com>
2011-03-01 00:48:01 +0530

    Archive Titan logs and reports by moving them to RESULTS_DIR

--- run_tests.sh
+++ run_tests.sh
@@ -9,8 +9,15 @@ cd $WORKING_DIR
 cp $selected_tests $TITAN_DIR/settings/titan-selectedtests.xml
 cd $TITAN_DIR
 
+rm -f $TREPORTPATH/pdf/*.pdf
+rm -f $TREPORTPATH/txt/*.txt
+
 log_init_testsuite $tsname $TARGET_NAME 
 
 $TITAN_DIR/start.sh cmd $tsname | tee $RESULTS_DIR/test_log_$tsname_$TARGET_NAME".txt"
+
+mv `ls -1 $TREPORTPATH/pdf/*.pdf` $RESULTS_DIR/test_report_$tsname_$TARGET_NAME".pdf"
+mv `ls -1 $TREPORTPATH/txt/*.txt` $RESULTS_DIR/test_result_$tsname_$TARGET_NAME".txt"
+ls -al $RESULTS_DIR
 
 log_finish_testsuite $tsname $TARGET_NAME 
