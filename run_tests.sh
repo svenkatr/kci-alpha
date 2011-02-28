91d64092ef917de45a6f471fb807e08663b4363f
Venkatraman S <svenkatr@gmail.com>
2011-02-28 12:19:07 +0530

    Provide proper testpackage name

--- run_tests.sh
+++ run_tests.sh
@@ -3,13 +3,14 @@
 #Get the absolute path of the list of testcases to execute
 #For example, $KCI_ROOT/testsuite/bootuptests
 selected_tests=$1
+tsname=`basename $selected_tests`
 
 cd $WORKING_DIR
 cp $selected_tests $TITAN_DIR/settings/titan-selectedtests.xml
 cd $TITAN_DIR
 
-log_init_testsuite `basename $selectedtests` $TARGET_NAME 
+log_init_testsuite $tsname $TARGET_NAME 
 
-start.sh cmd `basename $selectedtests`
+$TITAN_DIR/start.sh cmd $tsname | tee $RESULTS_DIR/test_log_$tsname_$TARGET_NAME".txt"
 
-log_finish_testsuite `basename $selectedtests` $TARGET_NAME 
+log_finish_testsuite $tsname $TARGET_NAME 
